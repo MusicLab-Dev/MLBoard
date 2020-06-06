f541ef873287a9bf8033564d0aaff6b139da1ced
Matthieu Moinvaziri <m.moinvaziri@gmail.com>
2020-06-07 00:16:13 +0200

    Updated unit tests

--- Benchmarks/MLBoardBenchmarks.cmake
+++ Benchmarks/MLBoardBenchmarks.cmake
@@ -11,6 +11,6 @@ add_executable(${CMAKE_PROJECT_NAME} ${MLBoardBenchmarksSources})
 
 target_link_libraries(${CMAKE_PROJECT_NAME}
 PUBLIC
-    benchmark
+    benchmark::benchmark
     MLBoardLib
 )
--- Tests/MLBoardTests.cmake
+++ Tests/MLBoardTests.cmake
@@ -3,7 +3,6 @@ project(MLBoardTests)
 set(MLBoardTestsDir ${MLBoardDir}/Tests)
 
 set(MLBoardTestsSources
-    ${MLBoardTestsDir}/Main.cpp
     ${MLBoardTestsDir}/tests_Board.cpp
 )
 
@@ -13,6 +12,6 @@ add_test(NAME ${CMAKE_PROJECT_NAME} COMMAND ${CMAKE_PROJECT_NAME})
 
 target_link_libraries(${CMAKE_PROJECT_NAME}
 PUBLIC
-    gtest
+    GTest::GTest GTest::Main
     MLBoardLib
 )
